e6953462ac8a735df63bbad44b007a5c61c86941
Blair-Mckenzie <wdb17134@uni.strath.ac.uk>
2021-09-30 18:28:51 +0100

    Lab finished, implemented using timer_A and interrupts

--- main.c
+++ main.c
@@ -1,52 +1,103 @@
-
-#include "io430.h"
+#include <io430.h>
 #define LED2 BIT6
 #define LED3 (BIT1+ BIT3 + BIT5)
 #define RED (BIT1)
 #define YELLOW (BIT1 + BIT3)
 #define SWITCH BIT3
-#define TIMER (3u * (32768/8)) // 3 seconds
+#define TIMER 12288                     // -- 3 seconds
+#define FLASHRATE 80000                 // 80 flashes/min
+#define LED_DELAY 65000                 // ~~ 0.75s delay
 
-void configureTimerA(void);
-//unsigned int count;
+// Function Declarations
+void setUpIO(void);
+void configureTimer(void);
+void startTimer(void);
+void startAndConfigureFlashing(void);
+void driver(void);
 
-int main( void ){
-  WDTCTL = WDTPW + WDTHOLD;     // Stop watchdog timer to prevent time out reset
-  
-  P1DIR |= LED2;                // Set P1.6 to output direction
-  P2DIR |= LED3;                // Set P2.1,P2.3,P2.5 to output direction
-  P1OUT = 0;
-//  _enable_interrupts();
-  configureTimerA();
+unsigned int i;                             //Global counter
+
+int main(void){
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  setUpIO();                                 
+  i=0;
+  __bis_SR_register(GIE);                   // Enter LPM3 w/ interrupt
+  configureTimer();
+  driver();                               // Main control of program
   return 0;
 }
 
-void configureTimerA(void){
-  // TASSELx bits 9-8, IDx bits 7-6, MCx bits 5-4
-  
-  TACTL = TASSEL_1 | ID_3 | MC_1| TAIE;         // ACLK as clock source, divider 8, countup, interrupt enabled
-  TACCR0 = TIMER;                               //The timer repeatedly counts from zero to the value of TACCR0.
-  TACCR1 = 0;           
-  TACCTL0 = CCIE;                               // Enable interrupt
-  TACCTL1 = CCIE;                               // Enable interrupt
- __bis_SR_register(GIE); 
-  LPM1;         //CPU, MCLK are disabled, ACLK is active
-}
-
-#pragma vector = TIMER0_A1_VECTOR
-__interrupt void Timer_A(void){
-  switch(TAIV){
-    case 0x02:
-      for (;;){
-        if((P1IN & SWITCH)==0){
-          P2OUT |= YELLOW;
-    //      P2OUT |= RED;
-        }else{
-          P2OUT = 0;
-        }
-      } 
-//    P2OUT = 0;
-    TACTL = MC_0;
-    break;
+void driver(void){
+  for (;;){
+    if((P1IN & SWITCH)==0){                 // Check the switch is depressed
+      P2OUT |= YELLOW;                      // Set the RGB Led to yellow
+      startTimer();                         // Start the 3 second timer
+    }else{
+      P2OUT &= ~LED3;                       // P2.1,P2.3,P2.5 LED Off                             
+      P1OUT &= ~LED2;                       // P1.6 LED Off
+      CCR0 =0;                              // Timer Off
+      configureTimer();                     // Reset Configuration
+      i=0;                                  // Reset Global Counter
+    }
   }
 }
+
+void setUpIO(void){
+//  P1REN |= BIT3; 			    // P1.3 Enable Pullup/Pulldown
+//  P1OUT = BIT3; 			    // P1.3 Pullup
+//  P1IE |= BIT3;				    // P1.3 Enable Interrupt
+//  P1IES |= BIT3; 			    // P1.3 Hi/Lo edge
+//  P1IFG &= ~BIT3; 			    // P1.3 IFG cleared
+  P1DIR |= LED2;                            // P1.6 Output
+  P1OUT &= ~LED2; 		            // P1.6 LED Off
+  P2DIR |= LED3;                            // P2.1,P2.3,P2.5 output
+  P2OUT &= ~ LED3;                          // P2.1,P2.3,P2.5 LED Off
+  return;
+}
+
+void configureTimer(void){
+    CCTL0 = CCIE;                             // CCR0 interrupt enabled
+    CCR0 = (TIMER) -1;                        // Set CCR0 to 3 seconds
+    __bis_SR_register(GIE);                   // Global Enable interrupt
+    return;
+}
+
+void startTimer(void){
+  TACTL = TASSEL_1 + ID_3+  MC_1;           // ACLK Clock Source (32768Hz), Divider of 8, UpMode
+}
+
+void startAndConfigureFlashing(void){
+  CCTL0 = CCIE;                             // CCR0 interrupt enabled
+  CCR0 = (FLASHRATE)-1;                     // CCR0 set to toggle rate of 80/minute
+  TACTL = TASSEL_1 +  MC_1;                 // ACLK Clock Source, UpMode
+   __bis_SR_register(LPM3_bits + GIE);      // Enter LPM3 w/ interrupt
+  return;
+}
+
+// Timer A0 interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer_A (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  i++;
+   if ((P1IN & SWITCH)==0){
+    if(i == 1) {                                  // First Time Interrupt fires (i.e. after three seconds)
+       CCR0 = 0;                                  // Timer Off
+       P2OUT &= ~LED3;                            // P2.1,P2.3,P2.5 LED Off 
+       startAndConfigureFlashing();               // Different timer setup for flashing
+    }
+   P1OUT ^= LED2;                               // P1.6 LED on
+   for(i=0; i < LED_DELAY; i++);                // Delay for alternating LEDs 
+   P2OUT ^= RED;                                // P2.1 LED on (Red)
+  }else{
+    driver();                                   // Back to main loop
+    P2OUT &= ~LED3;                             // P2.1,P2.3,P2.5 LED Off                             
+    P1OUT &= ~LED2;                             // P1.6 LED Off
+  }                              
+}
+
