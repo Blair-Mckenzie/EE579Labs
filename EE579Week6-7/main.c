d79b67f6260db8e610b5bfffc567fbeb8ddab7fd
Blair-Mckenzie <wdb17134@uni.strath.ac.uk>
2021-10-26 22:53:51 +0100

    Small changes

--- EE579Week6-7/main.c
+++ EE579Week6-7/main.c
@@ -3,39 +3,80 @@
 
 int main(void)
 {
-  WDTCTL = WDTPW + WDTHOLD;                   // Stop WDT
-  ADC10CTL0 = ADC10SHT_2 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
-  ADC10CTL1 = INCH_1;                         // input A1
-  ADC10AE0 |= 0x02;                           // PA.1 ADC option select
-  P1DIR |= 0x01;                              // Set P1.0 to output direction
-  P2DIR |= 0x30;                              // Set P2.4 and P2.5 to output direction
-  P2OUT |= 0x20;                              // Start P2.5 High
-  P2OUT &= ~0x10;                             // Start P2.4 Low
-  CCTL0 = CCIE;                               // Enable CCR0 Interrupt
-  CCR0 = 500;                                 // Toggle every 0.5ms for 1kHz
-  TACTL = TASSEL_2 + MC_1;                    // SMCLK source, Upmode
-//  __bis_SR_register(LPM0_bits + GIE);
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  ADC10CTL0 = SREF_1 + ADC10SHT_2 + REFON + ADC10ON + ADC10IE;
+  __enable_interrupt();                     // Enable interrupts.
+  TACCR0 = 30;                              // Delay to allow Ref to settle
+  TACCTL0 |= CCIE;                          // Compare-mode interrupt.
+  TACTL = TASSEL_2 | MC_1;                  // TACLK = SMCLK, Up mode.
+  LPM0;                                     // Wait for delay.
+  TACCTL0 &= ~CCIE;                         // Disable timer Interrupt
+  __disable_interrupt();
+  ADC10CTL1 = INCH_1;                       // input A1
+  ADC10AE0 |= 0x02;                         // PA.1 ADC option select
+  P1DIR |= 0x01;                            // Set P1.0 to output direction
 
   for (;;)
   {
-    ADC10CTL0 |= ENC + ADC10SC;      // Sampling and conversion start
-    __bis_SR_register(CPUOFF + GIE); // LPM0, ADC10_ISR will force exit
-    if (ADC10MEM < 0x1FF)
-      P1OUT &= ~0x01; // Clear P1.0 LED off
+    ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+    __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exi
+    if (ADC10MEM < 0x88)                    // ADC10MEM = A1 > 0.2V?
+      P1OUT &= ~0x01;                       // Clear P1.0 LED off
     else
-      P1OUT |= 0x01; // Set P1.0 LED on
+      P1OUT |= 0x01;                        // Set P1.0 LED on
   }
 }
 
 // ADC10 interrupt service routine
-#pragma vector = ADC10_VECTOR
-__interrupt void ADC10_ISR(void)
+#pragma vector=ADC10_VECTOR
+__interrupt void ADC10_ISR (void)
 {
-  __bic_SR_register_on_exit(CPUOFF); // Clear CPUOFF bit from 0(SR)
+  __bic_SR_register_on_exit(CPUOFF);        // Clear CPUOFF bit from 0(SR)
 }
 
-#pragma vector = TIMER0_A0_VECTOR
-__interrupt void Timer_A(void)
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void ta0_isr(void)
 {
-  P2OUT ^= 0x30; //Toggle P2.4 and P2.5
+  TACTL = 0;
+  LPM0_EXIT;                                // Exit LPM0 on return
 }
+
+
+//int main(void)
+//{
+//  WDTCTL = WDTPW + WDTHOLD;                   // Stop WDT
+//  ADC10CTL0 = ADC10SHT_2 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
+//  ADC10CTL1 = INCH_1;                         // input A1
+//  ADC10AE0 |= 0x02;                           // PA.1 ADC option select
+//  P1DIR |= 0x01;                              // Set P1.0 to output direction
+//  P2DIR |= 0x30;                              // Set P2.4 and P2.5 to output direction
+//  P2OUT |= 0x20;                              // Start P2.5 High
+//  P2OUT &= ~0x10;                             // Start P2.4 Low
+//  CCTL0 = CCIE;                               // Enable CCR0 Interrupt
+//  CCR0 = 500;                                 // Toggle every 0.5ms for 1kHz
+//  TACTL = TASSEL_2 + MC_1;                    // SMCLK source, Upmode
+////  __bis_SR_register(LPM0_bits + GIE);
+//
+//  for (;;)
+//  {
+//    ADC10CTL0 |= ENC + ADC10SC;      // Sampling and conversion start
+//    __bis_SR_register(CPUOFF + GIE); // LPM0, ADC10_ISR will force exit
+//    if (ADC10MEM < 0x1FF)
+//      P1OUT &= ~0x01; // Clear P1.0 LED off
+//    else
+//      P1OUT |= 0x01; // Set P1.0 LED on
+//  }
+//}
+//
+//// ADC10 interrupt service routine
+//#pragma vector = ADC10_VECTOR
+//__interrupt void ADC10_ISR(void)
+//{
+//  __bic_SR_register_on_exit(CPUOFF); // Clear CPUOFF bit from 0(SR)
+//}
+//
+//#pragma vector = TIMER0_A0_VECTOR
+//__interrupt void Timer_A(void)
+//{
+//  P2OUT ^= 0x30; //Toggle P2.4 and P2.5
+//}
