f8d2561071982cf763af0dec8e657bc2e5e99c95
Blair-Mckenzie <wdb17134@uni.strath.ac.uk>
2021-11-22 10:55:15 +0000

    Small changes

--- EE579Week10-11/main.c
+++ EE579Week10-11/main.c
@@ -1,34 +1,56 @@
 #include <io430.h>
+#define LED2 BIT6
+#define LED3 (BIT1 + BIT3 + BIT5)
 #define SWITCH BIT3
 #define BUZZEROUT 0x11
-#define FLASHRATE 11718  // 80 flashes/min
+#define YELLOW (BIT1 + BIT3)
+#define THREESECONDS 46875     // -- 3 seconds
+#define FLASHRATE 5860  // 80 flashes/min
 
 int count = 0;
-int freq1 = 750;
-int freq2 = 1500;
+int freq1 = 93;
+int freq2 = 187;
 int currentFreq = 0;
 
 void IO_init(void);
 void switch_init(void);
 void timer_init(void);
+void three_second_timer();
+void startFlashing();
 
 void main(void)
 {
   WDTCTL = WDTPW + WDTHOLD; // Stop WDT
   //Call initialise functions
-//  BCSCTL2 |= SELS + DIVS_2;              // divide clk by 8
+  BCSCTL2 |= DIVS_3;              // divide smclk by 8
   IO_init();
-  switch_init();
+//  switch_init();
   timer_init();
-  __bis_SR_register(LPM0_bits + GIE); // Enter LPM0 w/ interrupt
+  __bis_SR_register(GIE); // Enter LPM0 w/ interrupt
+  for(;;)
+  {
+    if ((P1IN & SWITCH) == 0)
+    {
+      P2OUT |= YELLOW;
+      three_second_timer();
+    }
+    else
+    {
+      P2OUT &= ~LED3;   // P2.1,P2.3,P2.5 LED Off
+      P1OUT &= ~LED2;   // P1.6 LED Off
+    }
+  }
 }
 
 void IO_init()
 {
   P2DIR |= BUZZEROUT; // P2.4, P2.5 outputs
+  P2DIR |= LED3;      // P2.1,P2.3,P2.5 output
   P1DIR |= BIT6;      // P1.6 output
   P2OUT |= 0x1;       // Start P2.0 High
   P2OUT &= ~0x10;     // Start P2.4 Low
+  P2OUT &= ~LED3;    // P2.1,P2.3,P2.5 LED Off
+//  P2OUT |= BIT1;      // Start P2.1 High
   P1OUT &= ~BIT6;     // Start P1.6 Low
 }
 
@@ -43,8 +65,23 @@ void switch_init()
 void timer_init()
 {
   TACCTL0 = CCIE; // TACCR0 toggle, interrupt enabled
-  CCR0 = 31250;    // 1MHz/8/31250 ~ 4Hz (period of 0.25s - 1/4=0.25)
+//  TACCTL1 = CCIE;                
+//  TACCTL2 = CCIE;                
+  CCR0 = 3906;    // 1MHz/8/31250 ~ 4Hz (period of 0.25s - 1/4=0.25)
+//  CCR1 = 5860;
+//  CCR2 = 3906;
   TACTL = TASSEL_2 + MC_1 + ID_3; // SMCLK, Upmode, /8
+}
+
+void three_second_timer()
+{
+  CCR1 = THREESECONDS;
+  __bis_SR_register(LPM0_bits + GIE); // Enter LPM0 w/ interrupt
+}
+
+void startFlashing()
+{
+  
 }
 
 //// Timer A0 interrupt service routine
@@ -56,8 +93,12 @@ __interrupt void Timer_A(void)
 //  {
 //    P1OUT ^= LED2; // P1.6 LED on
 //  }
+  if(CCR1 == THREESECONDS)
+  {
+    startFlashing();
+  }
   count += CCR0;              // Increment count with current value in CCR0
-  if (count >= 31250)          // Toggle rate of frequencies
+  if (count >= 3906)         // Toggle rate of frequencies
   {
     count = 0;                  
     if (currentFreq == 0)     //Check current frequency, switch CCR0 to have other frequency
@@ -74,23 +115,43 @@ __interrupt void Timer_A(void)
   P2OUT ^= BUZZEROUT;         // Toggle Buzzer Pins
 }
 
-#pragma vector = PORT1_VECTOR
-__interrupt void Port1_ISR(void)
-{
-  P1IFG &= ~SWITCH;     // Clear the interrupt flag for the switch
-  P1IE &= ~SWITCH;      // Disable Button interrupt
-  WDTCTL = WDT_MDLY_32; // Start and set watchdog timer (WDT) to trigger every 32ms
-  IFG1 &= ~WDTIFG;      // Clear the interrupt flag for the WDT
-  IE1 |= WDTIE;         // enable WDT interrupt
-  P1OUT ^= BIT6;        // Toggle P1.6 
-}
+// Timer A1 Interrupt Vector (TA0IV) handler
+//#pragma vector=TIMER0_A1_VECTOR
+//__interrupt void Timer_A1(void)
+//{
+//  switch( TA0IV )
+//  {
+//  case  2:
+//    P2OUT ^= BIT1;
+//    P1OUT ^= BIT6;
+//    CCR1 += 5860;
+//    break;
+//  case  4:
+//    
+//           break;
+//  case 10: 
+//           break;
+//  default: break;
+//  }
+//}
 
-#pragma vector = WDT_VECTOR
-__interrupt void WDT_ISR(void)
-{
-  IE1 &= ~WDTIE;            // disable Watchdog timer (WDT) interrupt
-  IFG1 &= ~WDTIFG;          // clear WDT interrupt flag
-  WDTCTL = WDTPW + WDTHOLD; // put WDT back in hold state
-  P1IE |= SWITCH;           // Enable interrupts for the switch
-}
+//#pragma vector = PORT1_VECTOR
+//__interrupt void Port1_ISR(void)
+//{
+//  P1IFG &= ~SWITCH;     // Clear the interrupt flag for the switch
+//  P1IE &= ~SWITCH;      // Disable Button interrupt
+//  WDTCTL = WDT_MDLY_32; // Start and set watchdog timer (WDT) to trigger every 32ms
+//  IFG1 &= ~WDTIFG;      // Clear the interrupt flag for the WDT
+//  IE1 |= WDTIE;         // enable WDT interrupt
+////  P1OUT ^= BIT6;        // Toggle P1.6 
+//}
+//
+//#pragma vector = WDT_VECTOR
+//__interrupt void WDT_ISR(void)
+//{
+//  IE1 &= ~WDTIE;            // disable Watchdog timer (WDT) interrupt
+//  IFG1 &= ~WDTIFG;          // clear WDT interrupt flag
+//  WDTCTL = WDTPW + WDTHOLD; // put WDT back in hold state
+//  P1IE |= SWITCH;           // Enable interrupts for the switch
+//}
 
