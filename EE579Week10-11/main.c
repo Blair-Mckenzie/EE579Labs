cd1e299dc99708118067f7f48ceed4cf53e57566
Blair-Mckenzie <wdb17134@uni.strath.ac.uk>
2021-11-29 23:32:55 +0000

    Just need to get 5% duty cycle on LED when program is just running normally (button is not being pressed)

--- EE579Week10-11/main.c
+++ EE579Week10-11/main.c
@@ -19,17 +19,22 @@ unsigned int greenLedCount = 0;
 
 void IO_init(void);
 void startThreeSeconds();
-void configureTimer0();
 void configureTimer1();
 void startFlashing80();
 void driver();
+void setUpandStartWatchDog();
+void init();
 
-void main(void)
+int main(void)
 {
 //  WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+  init();
+  return 0;
+}
+void init()
+{
   i = 0;
-  WDTCTL = WDT_ADLY_250;
-  IE1 |= WDTIE;                         // Enable WDT interrupts in the status register
+  setUpandStartWatchDog();
   BCSCTL2 |= DIVS_3;                    // divide smclk by 8
   IO_init();
   __bis_SR_register(GIE);		// Enter Low power mode 0 with interrupts enabled
@@ -52,7 +57,6 @@ void driver()
       P1OUT &= ~LED2;   // P1.6 LED Off
       TA0CCR0 =0;
       TA1CCR0 =0;
-//      configureThreeSeconds();
       i = 0;
     }
   }
@@ -70,6 +74,12 @@ void IO_init()
   P1OUT &= ~LED2;       // Start P1.6 Low
 }
 
+void setUpandStartWatchDog()
+{
+  WDTCTL = WDT_ADLY_250;                // 250ms interval   
+  IE1 |= WDTIE;                         // Enable WDT interrupts in the status register
+}
+
 void configureTimer1()
 {
   TA1CCR0 |= BUZZERRATE;	        // Counter value
@@ -77,7 +87,6 @@ void configureTimer1()
   TA1CCR1 = FLASHRATE30;
   TA1CTL = TASSEL_2 + MC_1 + ID_3;      // SMCLK, Upmode, /8
 }
-
 
 void startThreeSeconds()
 {
@@ -95,7 +104,6 @@ void startFlashing80()
   TA0CTL = TASSEL_2 + MC_1 + ID_3; // SMCLK, Upmode, /8
   __bis_SR_register(LPM0_bits + GIE); // Enter LPM0 w/ interrupt
 }
-
 
 //// Timer0 A0 interrupt service routine
 #pragma vector = TIMER0_A0_VECTOR
@@ -121,8 +129,7 @@ __interrupt void Timer0_A0(void)
   {
     P2OUT &= ~LED3; // P2.1,P2.3,P2.5 LED Off
     P1OUT &= ~LED2; // P1.6 LED Off
-    WDTCTL = WDT_ADLY_250;
-    driver();
+    init();
   }
 }
 
@@ -150,7 +157,7 @@ __interrupt void Timer0_A0(void)
   }
   else
   {
-    driver();
+    init();
   }
 }
 #pragma vector = WDT_VECTOR  // Interrupt Service Routine (ISR) for Watchdog Timer
